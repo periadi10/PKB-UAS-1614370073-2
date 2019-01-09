c4ff5132596bfb9d1d4a36a6d36d38a46fdde0cc
periadi10 <46518130+periadi10@users.noreply.github.com>
2019-01-09 16:08:37 +0700

    Add files via upload

---
+++ NOT_periadi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	int w1,x1,O;
+	w1=-1;
+	cout<<"X1=";
+	cin>>x1;
+	O=x1*w1;
+	if(O>=-0.5)
+	cout<<"Output NOT=top1"<<endl;
+else
+	cout<<"Output NOT=top2"<<endl;
+}
